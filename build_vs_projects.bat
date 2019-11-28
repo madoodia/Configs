7ec5e921fb2ba00425dcb2f215561b335504b008
Mahmoodreza Aarabi <madoodia@gmail.com>
2019-11-28 05:42:56 -0800

    a few edits on some paths

--- build_vs_projects.bat
+++ build_vs_projects.bat
@@ -4,5 +4,5 @@ rem ..................
 
 rem SET PATH="D:\Program Files\Autodesk\Maya2017\bin";%PATH%
 rem SET MAYA_LOCATION="D:\Program Files\Autodesk\Maya2017"
-rem SET DEVKIT_LOCATION=D:\Madoodia\_SDKs\_maya_sdks\Maya_2017
+rem SET DEVKIT_LOCATION=%SDKS_LOCATION%\_maya_sdks\Maya_2017
 START %comspec% /k "D:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat" x64 
