93eaece622effedb5eca8326790ae9dc5ed78c9b
Reza Aarabi <madoodia@gmail.com>
2023-09-27 16:52:34 -0700

    add custom commands

--- Windows Terminal/Microsoft.PowerShell_profile.ps1
+++ Windows Terminal/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,26 @@
 oh-my-posh init pwsh --config 'C:\Users\Chapool\AppData\Local\Programs\oh-my-posh\themes\madoodia.omp.json' | Invoke-Expression
+
+#  custom commands
+function dev {
+    cd D:\madoodia\dev
+}
+
+function sdks {
+    cd D:\madoodia\sdks
+}
+
+function UE {
+    cd D:\madoodia\dev\UnrealEngine
+}
+
+function pl {
+    cd D:\madoodia\dev\UE_Projects\UE5_PluginDev
+}
+
+function u {
+    cd ..
+}
+
+function ~ {
+    cd $home
+}
