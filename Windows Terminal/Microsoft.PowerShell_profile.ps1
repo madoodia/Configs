daa21cf6d9e8b11f1b4d9ac222bd18381778a672
Reza Aarabi <madoodia@gmail.com>
2023-11-25 09:53:34 -0800

    Update PROFILE Command

--- Windows Terminal/Microsoft.PowerShell_profile.ps1
+++ Windows Terminal/Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,24 @@
-oh-my-posh init pwsh --config 'C:\Users\Chapool\AppData\Local\Programs\oh-my-posh\themes\madoodia.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'C:\\Users\\Chapool\\AppData\\Local\\Programs\\oh-my-posh\\themes\\madoodia.omp.json' | Invoke-Expression
 
 #  custom commands
 function dev {
-    cd D:\madoodia\dev
+    cd "D:\\madoodia\\dev"
+}
+
+function pkgs {
+    cd "D:\\madoodia\\pkgs"
 }
 
 function sdks {
-    cd D:\madoodia\sdks
+    cd "D:\\madoodia\\sdks"
 }
 
 function UE {
-    cd D:\madoodia\dev\UnrealEngine
+    cd "D:\\madoodia\\dev\\UnrealEngine"
 }
 
 function upd {
-    & 'D:\madoodia\dev\UE_Projects\UE5_PluginDev\UE5_PluginDev.code-workspace'
+    & 'D:\\madoodia\\dev\\UE_Projects\\UE5_PluginDev\\UE5_PluginDev.code-workspace'
 }
 
 function u {
@@ -22,11 +26,11 @@ function u {
 }
 
 function ~ {
-    cd $home
+    cd "$home"
 }
 
 function mci {
-    code D:\madoodia\dev\madoodia_info;
+    code "D:\\madoodia\\dev\\madoodia_info;"
 }
 
 function pro {
@@ -34,19 +38,25 @@ function pro {
 }
 
 function ghp {
-    & 'D:\__GAME_PROJECTS__\RetroPixelDigital\Green-Hawk-Platoon.Game\GHP_Beta.code-workspace'
+    & 'D:\\__GAME_PROJECTS__\\RetroPixelDigital\\Green-Hawk-Platoon.Game\\GHP_Beta.code-workspace'
 }
 
 function conf {
-    code D:\madoodia\dev\Configs
+    code "D:\\madoodia\\dev\\Configs"
+}
+
+function stc {
+    Copy-Item $PROFILE -Destination "D:\\madoodia\\dev\\Configs\\Windows Terminal\\Microsoft.PowerShell_profile.ps1" -Recurse -force;
+    cd "D:\\madoodia\\dev\\Configs"; git add .;git commit -m "Update PROFILE Command";git push;
 }
 
 # ------------------------------------- HELP ---------------------------------------
 function -h {
     echo -----------------------
     echo 'dev: Open dev folder'
+    echo 'pkgs : Open pkgs folder'
     echo 'sdks : Open sdks folder'
-    echo 'UE: Open dev\UnrealEngine'
+    echo 'UE: Open dev\\UnrealEngine'
     echo 'upd: Open UE5_PluginDev in VSCode'
     echo 'u: cd ..'
     echo '~: cd $home'
@@ -55,5 +65,6 @@ function -h {
     echo 'pro: Open $PROFILE in VSCode'
     echo 'ghp: Open GHP_Beta in VSCode'
     echo 'conf: Open Configs in VSCode'
+    echo 'stc: Copy $PROFILE into config and push changes'
     echo -----------------------
 }
