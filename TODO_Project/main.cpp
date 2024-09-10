26496a16b4fa1fdad88ded9fa0d47192873e05db
Reza Aarabi <madoodia@gmail.com>
2024-09-10 13:35:22 -0700

    Update PROFILE Command

---
+++ TODO_Project/main.cpp
@@ -0,0 +1,19 @@
+#include <windows.h>
+
+#define MAX_THREADS 2
+
+static int integer = 0;
+
+// TODO: Add your code here
+
+// FIXME: Implement the function
+
+// IMPORTANT: Do not modify the main function
+
+
+int main()
+{
+    int TODO = 0;
+    int FIXME = 0;
+    return 0;
+}
